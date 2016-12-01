733ab0285a2fa314569324e49afe2b6f10b41f0f
Gaute Linga <gaute.linga@gmail.com>
2016-12-01 14:24:45 +0100

    Modifications.

---
+++ make_movie.py
@@ -0,0 +1,23 @@
+import os
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Convert sequence of images to video.")
+    parser.add_argument("folder", type=str, help="Folder with png files.")
+    parser.add_argument("-framerate", type=int, help="Framerate.", default=25)
+    args = parser.parse_args()
+
+    #os.system("ls")
+    
+    os.system("ffmpeg -framerate {framerate} "
+              "-i {folder}/uz_%d.png"
+              " -qscale:v 0"
+              " {folder}/uz.mp4".format(
+                  folder=args.folder,
+                  framerate=args.framerate))
+
+
+if __name__ == "__main__":
+    main()
--- pyturb.py
+++ pyturb.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import argparse
 import os
 from mpi4py import MPI
+import copy
 
 
 def get_settings():
@@ -24,8 +25,10 @@ def get_settings_serial():
                         help="HDF file for position of probes.")
     parser.add_argument("u_folder", type=str, help="Folder containing u.")
     parser.add_argument("out_folder", type=str, help="Output folder.")
-    parser.add_argument("u_id_start", type=int, help="u field start id.")
-    parser.add_argument("u_id_stop", type=int, help="u field stop id.")
+    parser.add_argument("--u_id_max", type=int, help="u field max id.",
+                        default=None)
+    parser.add_argument("-a", "--append", action="store_true",
+                        help="append to file")
     args = parser.parse_args()
     return args
 
@@ -36,10 +39,14 @@ def main():
     comm = MPI.COMM_WORLD
     mpi_size = comm.Get_size()
     mpi_rank = comm.Get_rank()
-    
+
     nu = 9e-6
     dx = 2
     dt = 150 * 0.2
+
+    outfilename = os.path.join(args.out_folder, "output.h5")
+    if args.append:
+        assert os.path.exists(outfilename)
 
     with h5py.File(args.x_file, "r") as h5f:
         pos = np.array(h5f["x"])
@@ -54,7 +61,8 @@ def main():
             np.all([pos[:, (i+1) % 3] == 0.,
                     pos[:, (i+2) % 3] == 0.], axis=0))
 
-    print dim_x
+    if mpi_rank == 0:
+        print "Dimensions:", dim_x
     dims = (dim_x[0], dim_x[1], dim_x[2])
 
     x = dict()
@@ -69,10 +77,25 @@ def main():
         u[i] = np.zeros_like(x[i])
         du[i] = np.zeros_like(x[i])
 
-    num_series = args.u_id_stop + 1 - args.u_id_start
+    _, _, filenames = os.walk(args.u_folder).next()
+    u_ids = []
+    for filename in filenames:
+        file_id = int(filename.split("_")[1].split(".")[0])
+        u_ids.append(file_id)
 
-    series_proc = range(args.u_id_start + mpi_rank,
-                        args.u_id_stop+1, mpi_size)
+    u_ids.sort()
+
+    if args.append:
+        with h5py.File(outfilename, "r") as h5fapp:
+            num_series_already = len(h5fapp["uz_centerline"])
+            uz_centerline_already = copy.copy(
+                np.array(h5fapp["uz_centerline"]))
+            q_already = copy.copy(np.array(h5fapp["q"]))
+            f_already = copy.copy(np.array(h5fapp["f"]))
+            u_mean_already = copy.copy(np.array(h5fapp["u_mean"]))
+
+    num_series = len(u_ids)
+    series_proc = range(mpi_rank, num_series, mpi_size)
     num_series_proc = len(series_proc)
 
     uz_centerline_series = np.zeros((num_series_proc, dims[2]-1))
@@ -80,79 +103,94 @@ def main():
     f_series = np.zeros((num_series_proc, dims[2]-1))
     u_mean_series = np.zeros((num_series_proc, 3))
 
-    for row_id, u_id in enumerate(series_proc):
+    for row_id_proc, row_id in enumerate(series_proc):
+        u_id = u_ids[row_id]
         u_id_str = str(u_id)
-        with h5py.File(args.u_folder + "/u_" + u_id_str + ".h5", "r") as h5f:
-            vel = np.asarray(h5f["u/" + u_id_str])
-        print "Imported u_" + u_id_str
+        if args.append and row_id < num_series_already:
+            print "Copying u_" + u_id_str
+            uz_centerline_series[row_id_proc, :] = \
+                uz_centerline_already[row_id, :]
+            q_series[row_id_proc, :] = q_already[row_id, :]
+            f_series[row_id_proc, :] = f_already[row_id, :]
+            u_mean_series[row_id_proc, :] = u_mean_already[row_id, :]
+        else:
+            with h5py.File(
+                    args.u_folder + "/u_" + u_id_str + ".h5", "r") as h5f:
+                try:
+                    vel = np.asarray(h5f["u/" + u_id_str])
+                except:
+                    exit("Couldn't find dataset u/" + u_id_str)
+            print "Imported u_" + u_id_str
 
-        for i in xrange(3):
-            u[i][:, :] = vel[:, i].reshape(dims)
-            u_mean[i] = np.mean(u[i][:-1, :-1, :-1])
-            du[i][:, :] = u[i][:, :] - u_mean[i]
+            for i in xrange(3):
+                u[i][:, :] = vel[:, i].reshape(dims)
+                u_mean[i] = np.mean(u[i][:-1, :-1, :-1])
+                du[i][:, :] = u[i][:, :] - u_mean[i]
 
-        # uz_mean_xy = np.mean(u[2][:, :, :], axis=2)
-        # duz_xy = u[2][:, :, dims[2]/2] - uz_mean_xy
+            # uz_mean_xy = np.mean(u[2][:, :, :], axis=2)
+            # duz_xy = u[2][:, :, dims[2]/2] - uz_mean_xy
 
-        plt.imsave(os.path.join(args.out_folder, "uz_" + u_id_str + ".png"),
-                   u[2][dims[0]/2, :, :],
-                   origin='lower',
-                   cmap=plt.get_cmap('viridis'))
+            plt.imsave(os.path.join(
+                args.out_folder, "uz_" + u_id_str + ".png"),
+                       u[2][dims[0]/2, :, :],
+                       origin='lower',
+                       cmap=plt.get_cmap('viridis'))
 
-        uz_centerline = np.mean(u[2][dims[0]/2-dx:dims[0]/2+dx+1,
-                                     dims[1]/2-dx:dims[1]/2+dx+1, :-1],
-                                axis=(0, 1))
+            uz_centerline = np.mean(u[2][dims[0]/2-dx:dims[0]/2+dx+1,
+                                         dims[1]/2-dx:dims[1]/2+dx+1, :-1],
+                                    axis=(0, 1))
 
-        q = np.sqrt(np.mean(
-            du[0][:-1, :-1, :-1]**2 + du[1][:-1, :-1, :-1]**2,
-            axis=(0, 1)))/u_mean[2]
+            q = np.sqrt(np.mean(
+                du[0][:-1, :-1, :-1]**2 + du[1][:-1, :-1, :-1]**2,
+                axis=(0, 1)))/u_mean[2]
 
-        ffric_l = np.mean(
-            (u[2][1, :-1, :-1]-u[2][0, :-1, :-1])/(coord[0][1]-coord[0][0]),
-            axis=0)
-        ffric_r = np.mean(
-            (u[2][-2, :-1, :-1]-u[2][-1, :-1, :-1]) /
-            (coord[0][-1]-coord[0][-2]),
-            axis=0)
-        ffric_b = np.mean(
-            (u[2][:-1, 1, :-1]-u[2][:-1, 0, :-1]) /
-            (coord[1][1]-coord[1][0]),
-            axis=0)
-        ffric_t = np.mean(
-            (u[2][:-1, -2, :-1]-u[2][:-1, -1, :-1]) /
-            (coord[1][-1]-coord[1][-2]),
-            axis=0)
-        ffric = (ffric_l + ffric_r + ffric_t)/3.
+            ffric_l = np.mean(
+                (u[2][1, :-1, :-1]-u[2][0, :-1, :-1]) /
+                (coord[0][1]-coord[0][0]),
+                axis=0)
+            ffric_r = np.mean(
+                (u[2][-2, :-1, :-1]-u[2][-1, :-1, :-1]) /
+                (coord[0][-1]-coord[0][-2]),
+                axis=0)
+            ffric_b = np.mean(
+                (u[2][:-1, 1, :-1]-u[2][:-1, 0, :-1]) /
+                (coord[1][1]-coord[1][0]),
+                axis=0)
+            ffric_t = np.mean(
+                (u[2][:-1, -2, :-1]-u[2][:-1, -1, :-1]) /
+                (coord[1][-1]-coord[1][-2]),
+                axis=0)
+            ffric = (ffric_l + ffric_r + ffric_t)/3.
 
-        uz_centerline_series[row_id, :] = uz_centerline[:]
-        q_series[row_id, :] = q[:]
-        f_series[row_id, :] = ffric[:]
-        u_mean_series[row_id, :] = [u_mean[0], u_mean[1], u_mean[2]]
+            uz_centerline_series[row_id_proc, :] = uz_centerline[:]
+            q_series[row_id_proc, :] = q[:]
+            f_series[row_id_proc, :] = ffric[:]
+            u_mean_series[row_id_proc, :] = [u_mean[0], u_mean[1], u_mean[2]]
 
-        # plt.figure()
-        # plt.plot(coord[2], q)
-        # plt.savefig(os.path.join(args.out_folder, "q_" + u_id_str + ".png"))
+            # plt.figure() plt.plot(coord[2], q)
+            # plt.savefig(os.path.join(args.out_folder, "q_" +
+            # u_id_str + ".png")
 
-        # plt.figure()
-        # plt.plot(coord[2], u_centerline)
-        # plt.savefig(os.path.join(args.out_folder, "uz_centerline_" +
-        # u_id_str + ".png"))
+            # plt.figure()
+            # plt.plot(coord[2], u_centerline)
+            # plt.savefig(os.path.join(args.out_folder, "uz_centerline_" +
+            # u_id_str + ".png"))
 
-        # plt.plot(coord[2],
-        #          np.mean(u_centerline) * np.ones_like(u_centerline))
-        # plt.plot(coord[2], duz)
+            # plt.plot(coord[2],
+            #          np.mean(u_centerline) * np.ones_like(u_centerline))
+            # plt.plot(coord[2], duz)
 
-        # plt.figure()
-        # plt.plot(coord[2], duz_l)
-        # plt.plot(coord[2], duz_r)
-        # plt.plot(coord[2], duz_t)
-        # plt.plot(coord[2], duz_b)
-        # plt.plot(coord[2], duz)
+            # plt.figure()
+            # plt.plot(coord[2], duz_l)
+            # plt.plot(coord[2], duz_r)
+            # plt.plot(coord[2], duz_t)
+            # plt.plot(coord[2], duz_b)
+            # plt.plot(coord[2], duz)
 
     # plt.show()
 
     h5f = h5py.File(
-        os.path.join(args.out_folder, "output.h5"),
+        outfilename,
         "w", driver="mpio", comm=comm)
 
     dset_q = h5f.create_dataset("q", (num_series, dims[2]-1))
