27a3da22ed1debc25e106b61002fb566ccf4d842
Gaute Linga <gaute.linga@gmail.com>
2018-07-26 20:10:20 +0200

    Added another.

---
+++ get_mean_and_std.py
@@ -0,0 +1,23 @@
+import argparse
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import os
+
+
+parser = argparse.ArgumentParser(description="Compute average and standard deviation of timeseries.")
+parser.add_argument("file", type=str, help="Timeseries file")
+args = parser.parse_args()
+
+data = np.loadtxt(args.file)
+t = data[:, 0]
+x = data[:, 1]
+
+print "Average:       ", x.mean()
+print "Standard dev.: ", x.std()
+
+if data.shape[1] > 2:
+    y = data[:, 2]
+    print "Average2:      ", y.mean()
+    print "Standard dev.2:", y.std()
