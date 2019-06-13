111bcd0862cd9dafdcf8563f5aa30f273f91e32f
Gaute Linga <gaute.linga@gmail.com>
2019-06-13 15:40:10 +0200

    Introduced low-memory mode.

--- analyze_fluctuations.py
+++ analyze_fluctuations.py
@@ -13,6 +13,7 @@ parser.add_argument("folder", type=str, help="Data folder")
 parser.add_argument("--noplot", action="store_true", help="Don't plot")
 parser.add_argument("-i_start", type=int, default=None, help="First id")
 parser.add_argument("-i_stop", type=int, default=None, help="Last id")
+parser.add_argument("--low_memory_mode", action="store_true", help="Low memory mode")
 args = parser.parse_args()
 
 with h5py.File(args.probes_file, "r") as h5f:
@@ -25,12 +26,39 @@ for filename in glob.glob(os.path.join(args.folder, "*.h5")):
             (args.i_stop is None or file_id < args.i_stop)):
         filenames[file_id] = filename
 
-data = []
-for file_id in sorted(filenames.keys()):
+print("Found all filenames")
+
+#data = []
+n_u = len(filenames)
+filename_keys = sorted(filenames.keys())
+
+def load_dset(i, verbose=True):
+    file_id = filename_keys[i]
     with h5py.File(filenames[file_id], "r") as h5f:
-        print("File:", file_id, "of", len(filenames))
-        u = np.array(h5f["u/"+str(file_id)])
-        data.append(u)
+        if verbose:
+            print("File: {} ({} of {})".format(file_id, i, n_u))
+        u = np.array(h5f["u/" + str(file_id)])
+        return u
+
+low_memory_mode = args.low_memory_mode
+if not low_memory_mode:
+    try:
+        data = np.zeros((coords.shape[0], 3, n_u))
+    except:
+        print("Turning on low memory mode...")
+        low_memory_mode = True
+
+if not low_memory_mode:
+    for i, file_id in enumerate(filename_keys):
+        u = load_dset(i)
+        #data.append(u)
+        data[:, :, i] = u
+
+def get_dset(i):
+    if low_memory_mode:
+        return load_dset(i)
+    else:
+        return data[:, :, i]
 
 do_plot = not args.noplot
 
@@ -44,20 +72,24 @@ x = coords[:, 0].reshape((Nx, Ny, Nz))
 y = coords[:, 1].reshape((Nx, Ny, Nz))
 z = coords[:, 2].reshape((Nx, Ny, Nz))
 
-u_mean = np.zeros_like(data[0])
-du2 = np.zeros(len(data[0]))
+u_mean = np.zeros_like(coords)
+du2 = np.zeros(len(coords))
 
-u_avg_t = np.zeros((len(data), 4))
-du2_t = np.zeros((len(data), 2))
-for dset in data:
-    u_mean[:, :] += dset[:, :]/len(data)
+u_avg_t = np.zeros((n_u, 4))
+du2_t = np.zeros((n_u, 2))
+#for dset in data:
+for i in range(n_u):
+    dset = get_dset(i)
+    u_mean[:, :] += dset[:, :]/n_u
 u_mean_avg = u_mean.mean(0)
-for i, dset in enumerate(data):
+for i in range(n_u):
+    dset = get_dset(i)
     du2_i = np.sum((dset[:, :]-u_mean)**2, 1)
-    du2[:] += du2_i/len(data)
+    du2[:] += du2_i/n_u
     du2_t[i, 0] = i
     du2_t[i, 1] = du2_i.mean()
-for i, dset in enumerate(data):
+for i in range(n_u):
+    dset = get_dset(i)
     u_avg_t[i, 0] = i
     u_avg_t[i, 1:] = dset[:, :].mean(0)
 
